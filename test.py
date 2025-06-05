7e5804a994373dad06c9e7ec317fb0ec6ad10707
Tan <jaylonetan@airts.sg>
2025-06-05 09:34:39 +0800

    Update tests

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 import numpy as np
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
 from activations import *
 from main import MyNeuralNetwork
 
@@ -31,6 +33,8 @@ y = np.array([
     0
 ])
 
+X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.8, random_state=42)
+
 n = 100  # Number of runs
 accuracies = []
 
@@ -44,13 +48,13 @@ for i in range(n):
     nn.Linear(6, 1, 'he')
     nn.Sigmoid()
     
-    # nn.train_mini_batch(X, y, num_of_epochs=10000, learning_rate=0.001, batch_size=5)
-    nn.train(X, y, num_of_epochs=10000, learning_rate=0.01)
+    nn.train_mini_batch(X, y, num_of_epochs=10000, learning_rate=0.1, batch_size=3, lambda_l=0.01, threshold=10000)
+    ## nn.train(X_train, y_train, num_of_epochs=10000, learning_rate=0.01, lambda_l=0.01, threshold=15)
 
     # Evaluate accuracy
-    y_pred = nn.forward(X)
+    y_pred = nn.forward(X_test)
     y_pred_class = (y_pred > 0.5).astype(int)
-    acc = np.mean(y_pred_class == y.reshape(-1, 1))
+    acc = np.mean(y_pred_class == y_test.reshape(-1, 1))
     accuracies.append(acc)
 
 # Print average accuracy
